a88a3a5684573a0338bf71a0ac1baab31a4f56ee
kevinbroberg <kevin.broberg@gmail.com>
2020-09-14 20:09:08 -0400

    Implement pagination

--- src/components/cards/CardList.vue
+++ src/components/cards/CardList.vue
@@ -1,22 +1,28 @@
 <template>
     <div>
         <div>
-            <multiselect v-model="nameFilter" :options="filteredCards.map(c => c.Name)" :close-on-select="true" :clear-on-select="false" 
-            :searchable="true" placeholder="Filter by name">
+            <multiselect v-model="nameFilter" :options="filteredCards.map(c => c.Name)" :close-on-select="true"
+                :clear-on-select="false" :searchable="true" placeholder="Filter by name">
             </multiselect>
         </div>
         <div>
             <multiselect v-model="symbolFilter" :options="symbolOptions" :multiple="true" :close-on-select="false" 
-            :clear-on-select="false" :searchable="false" placeholder="Filter by symbol">
+                :clear-on-select="false" :searchable="false" placeholder="Filter by symbol">
             </multiselect>
         </div>
         <div>
             <multiselect v-model="originFilter" :options="originOptions" :multiple="true" :close-on-select="false" 
-            :clear-on-select="false" :searchable="true" placeholder="Filter by set">
+                :clear-on-select="false" :searchable="true" placeholder="Filter by set">
             </multiselect>
         </div>
-        
-        <div class="cardgrid" :key="card.Name" v-for="card in filteredCards">
+        <div class="offset">
+            <nav aria-label="Page navigation example">
+                <button type="button" class="page-link" @click="page--" v-if="page != 1"> Previous </button>
+                <button type="button" class="page-link" @click="page = pageNumber" v-bind:key="pageNumber" v-for="pageNumber in pages.slice(page-1, page+5)"> {{pageNumber}} </button>
+                <button type="button" class="page-link" @click="page++" v-if="page < pages.length"> Next </button>
+            </nav>
+        </div>
+        <div class="cardgrid" :key="card.Name" v-for="card in paginatedCards">
             <CardDetail v-bind:card="card" /> <!-- v-show="allFiltersMatch(card)" -->
         </div>
     </div>
@@ -39,7 +45,9 @@ export default {
             originOptions: [],
             nameFilter: '',
             symbolFilter: [],
-            originFilter: []
+            originFilter: [],
+            page: 1,
+            perPage: 50,
         }
     },
     created() {
@@ -48,6 +56,14 @@ export default {
     computed: {
         filteredCards() {
             return this.cardData.filter(card => this.allFiltersMatch(card))
+        },
+        paginatedCards() {
+            const start = (this.page - 1) * this.perPage
+            return this.filteredCards.slice(start, start + this.perPage)
+        },
+        pages() {
+            const size = Math.ceil(this.filteredCards.length / this.perPage)
+            return [...Array(size).keys()].map(n => n+1)
         }
     },
     methods: {
@@ -80,5 +96,14 @@ export default {
 
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 <style scoped>
-    
+    button.page-link {
+        display: inline-block;
+        font-size: 20px;
+        color: #29b3ed;
+        font-weight: 500;
+    }
+    .offset{
+        width: 500px !important;
+        margin: 20px auto;  
+    }
 </style>
