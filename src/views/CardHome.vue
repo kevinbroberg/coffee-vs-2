0d7adc67231bab5ecba548698bef23bdaee656fa
kevinbroberg <kevin.broberg@gmail.com>
2020-09-28 19:23:58 -0400

    Cleanup CardHome page

--- src/views/CardHome.vue
+++ src/views/CardHome.vue
@@ -8,23 +8,14 @@
 import CardList from '@/components/cards/CardList'
 import rawCards from '@/assets/cards.json'
 
-console.log('I have ' + rawCards.length + ' raw cards')
 export default {
   name: 'Home',
   components: {
     CardList
   },
-  // TODO a stackoverflow answer suggested to avoid `data` for static content, due to overhead, but 
    data() {
        return { 
-        //  cards: rawCards
-          cards : rawCards.map( card => {
-            if (card.asset) {
-              // TODO We really ought to not crash the whole display if a card is missing
-              card.img = require('@/assets/card_images/' + card.asset);
-            }
-            return card
-         }) 
+          cards: rawCards // formerly did preprocessing here, now moved to CardList
     }
   }
 }
@@ -32,7 +23,7 @@ export default {
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
-    #app {
+  #app {
     font-family: Montserrat, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
@@ -40,14 +31,5 @@ export default {
     color: #2c3e50;
     margin-top: 60px;
     
-    }
-
-    .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-    }
+  }
 </style>
