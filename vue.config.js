ae096084b613336cea496eadfee73d13165642a9
kevinbroberg <kevin.broberg@gmail.com>
2020-10-05 22:23:30 -0400

    Put webpack back again, with a comment explaining why you'd turn it off

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
-module.exports = { 
-    chainWebpack(config) {
-        config.module.rule('images').use('url-loader')
-            .tap(options => {
-                options.limit = 0;
-                return options;
-            });
-    }
-}
+// un-commenting will stop webpack from optimizing bad Git LFS pointers into nonsense base64 strings
+//module.exports = {
+//    chainWebpack(config) {
+//        config.module.rule('images').use('url-loader')
+//            .tap(options => {
+//                options.limit = 0;
+//                return options;
+//            });
+//    }
+//}
