3e8c5d4bbd5dcaa4f179da9144efe9c50c25ee92
kevinbroberg <kevin.broberg@gmail.com>
2020-10-10 23:12:55 -0400

    Modularity! CardHome is now responsible for filtering, CardList replaceable

---
+++ src/components/cards/InfiniteScrollCardDetailList.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="scroll">
+        <list-scroller :item-component="item" 
+            :items-data="scrolledCards" :item-height="2" @bottom="addMore" />
+    </div>
+</template>
+
+<script>
+import CardDetail from './CardDetail.vue'
+import ListScroller from 'vue-list-scroller'
+
+export default {
+    name: "InfiniteScrollCardDetailList",
+    components: {
+        ListScroller
+    },
+    data() {
+        return {
+            scrollLimit: 3,
+            scrollPageSize: 10,
+            item: CardDetail
+        }
+    },
+    computed: {
+        scrolledCards() {
+            return this.filteredCards.slice(0, this.scrollLimit * this.scrollPageSize)
+        }
+    },
+    methods: {
+        addMore() {
+            this.scrollLimit = this.scrollLimit + 1
+        }
+    },
+    props: ["filteredCards"]
+}
+</script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+<style scoped>
+    button.page-link {
+        display: inline-block;
+        font-size: 20px;
+        color: #29b3ed;
+        font-weight: 500;
+    }
+    .offset{
+        width: 500px !important;
+        margin: 20px auto;  
+    }
+    .scroll {
+        overflow-anchor: none;
+    }
+</style>
--- src/views/CardHome.vue
+++ src/views/CardHome.vue
@@ -1,21 +1,102 @@
 <template>
-  <div id="app">
-    <CardList v-bind:cardData="cards"/>
+  <div id="app">    
+    <div>
+      <div class="input">
+        Regex filter:
+        <input v-model="regexNameFilter">
+      </div>
+      <div>
+        <multiselect v-model="selectedExactNames" :options="filteredCards.map(c => c.Name)" :close-on-select="false"
+            :clear-on-select="false" :searchable="true" placeholder="Filter by exact name">
+        </multiselect>
+      </div>
+      <div>
+        <multiselect v-model="selectedSymbols" :options="symbolOptions" :multiple="true" :close-on-select="false" 
+            :clear-on-select="false" :searchable="false" placeholder="Filter by symbol">
+        </multiselect>
+      </div>
+      <div>
+        <multiselect v-model="selectedOrigins" :options="originOptions" :multiple="true" :close-on-select="false" 
+            :clear-on-select="false" :searchable="true" placeholder="Filter by set">
+        </multiselect>
+      </div>
+      <InfiniteScrollCardDetailList v-bind:filteredCards="filteredCards"/>
+    </div>
   </div>
 </template>
 
 <script>
-import CardList from '@/components/cards/CardList'
-import rawCards from '@/assets/cards.json'
+import InfiniteScrollCardDetailList from '@/components/cards/InfiniteScrollCardDetailList'
+import Multiselect from 'vue-multiselect'
+import cards from '@/assets/cards.json'
 
 export default {
   name: 'Home',
   components: {
-    CardList
+    Multiselect,
+    InfiniteScrollCardDetailList
   },
-   data() {
-       return { 
-          cards: rawCards // formerly did preprocessing here, now moved to CardList
+  data() {
+    return {
+      // TODO do these belong elsewhere?
+      symbolOptions: ["Air", "All", "Chaos", "Death", "Earth", "Evil", "Fire", "Good", "Infinity", "Life", "Order", "Void", "Water"],
+      originOptions: [],
+      regexNameFilter: '',
+      selectedExactNames: [],
+      selectedSymbols: [],
+      selectedOrigins: [],
+      cardData: cards
+    }
+  },
+  created() {
+      this.originOptions = [...new Set(this.cardData.map(card => card.Set))]
+  },
+  computed: {
+    filteredCards() {
+      return this.cardData.filter(card => this.allFiltersMatch(card)).map(this.decorateWithImg)
+    }
+  },
+  methods: {
+    decorateWithImg(card) {
+      if (card.asset && !card.img) {
+        // TODO We really ought to not crash the whole display if a card is missing
+        card.img = require('@/assets/card_images/' + card.asset);
+      }
+      return card
+    },
+    regexMatchFilter(card) {
+      if(this.regexNameFilter) {
+        const regex = new RegExp(".*" + this.regexNameFilter + ".*", 'i')
+        return regex.test(card.Name)
+      } else {
+        return true
+      }
+    },
+    symbolMatchFilter(card) {
+      if (this.selectedSymbols && this.selectedSymbols.length > 0) {
+        return card.Resources.some(sym => this.selectedSymbols.includes(sym))
+      } else {
+        return true
+      }
+    },
+    originMatchFilter(card) {
+      // names like "setFilter" seemed to get misunderstood by javascript lmao
+      if (this.selectedOrigins && this.selectedOrigins.length > 0) {
+        return this.selectedOrigins.includes(card.Set)
+      } else {
+        return true
+      }
+    },
+    exactNameFilter(card) {
+      if (this.selectedExactNames && this.selectedExactNames.length > 0) {
+        return this.selectedExactNames.includes(card.Name)
+      } else {
+        return true
+      }
+    },
+    allFiltersMatch(card) {
+      let filters = [this.regexMatchFilter, this.originMatchFilter, this.symbolMatchFilter, this.exactNameFilter]
+      return filters.every(f => f(card))
     }
   }
 }
