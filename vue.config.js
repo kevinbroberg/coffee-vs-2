d00c0cffe73d684bb62fc2855dd51ae073948e13
kevinbroberg <kevin.broberg@gmail.com>
2020-10-10 20:39:55 -0400

    Turn netpack off to doublecheck that netlify really is borked

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 // un-commenting will stop webpack from optimizing bad Git LFS pointers into nonsense base64 strings
-//module.exports = {
-//    chainWebpack(config) {
-//        config.module.rule('images').use('url-loader')
-//            .tap(options => {
-//                options.limit = 0;
-//                return options;
-//            });
-//    }
-//}
+module.exports = {
+    chainWebpack(config) {
+        config.module.rule('images').use('url-loader')
+            .tap(options => {
+                options.limit = 0;
+                return options;
+            });
+    }
+}
