5cbf8ac64d39f1855b0807df2ec85dd8ca1238cd
kevinbroberg <kevin.broberg@gmail.com>
2020-11-28 22:13:25 -0500

    About page improvements

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="about">
-    <h1>CoffeeVS helps you find cards and build decks for the card game, Universus</h1>
-    <p>Some features</p>
+    <h1>CoffeeVS helps you find cards and build decks for Universus</h1>
+    <p>Search features</p>
     <ul>
-    <li>The Name filter supports direct matches as well as regular expressions </li>
-    <li>Name recommendations are filtered by the other filters as well</li>
-    <li>Click on a card to add a copy to your deck; rightclick to remove</li>
+    <li>The Name filter supports direct matches as well as regular expressions. Press Enter to add your regex to the list of search options</li>
+    <li>You will only see cards that match ALL your filters</li>
+    <li>Set, Card Type, and Symbol filters allow multiple selections, and show cards that match ANY of those selected</li>
+    <li>Two Symbol filters allow you to combine AND / OR matching</li>
+    <li>For example, to find cards with Earth AND either Life OR Order, select Earth with one, and Life + Order in the other</li>
+    <p>Deckbuilding</p>
+    <li>On the search/home page, click on a card to add a copy to your deck; rightclick to remove</li>
+    <li>You cannot add cards to more than 4 copies; if you remove the last copy, the card will leave your deck entirely</li>
+    <li>On the Deck page, you can adjust quantities with the buttons in the "Qty" row</li>
+    <li>You can save your deck offline with the "Download as .txt" button. Cloud storage coming... eventually</li>
     </ul>
-    <p>Your deck and you</p>
-        <ul>
-        <li>Click on a card to add another copy, or click the Remove button to remove all copies</li>
-        <li>Click on a card to add a copy to your deck; rightclick to remove</li>
-        </ul>
+    <p>Formats:</p>
+    <ul><li>Standard includes recent promos, DLC, and sets dating back to Battle for Power</li>
+        <li>Retro dates to Red Horizon, and includes a variety of old promos, and also includes all of Standard</li>
+        <li>Future solely includes unreleased promos (Princess and the Pea)</li>
+        <li>Playtest solely includes cards released for Community Playtest on Tabletop Simulator. Updated 28 Nov 2020</li>
+        <li>Universal theoretically starts with ShadoWar, but I didn't bother loading those sets. It only includes a few very old promos</li>
+    </ul>
   </div>
 </template>
