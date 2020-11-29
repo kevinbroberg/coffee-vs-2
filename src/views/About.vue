c8200fa4ee278491fc9f3ad4a183fae47387f1e4
kevinbroberg <kevin.broberg@gmail.com>
2020-11-28 22:49:07 -0500

    More about page improvements

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,11 +3,13 @@
     <h1>CoffeeVS helps you find cards and build decks for Universus</h1>
     <p>Search features</p>
     <ul>
-    <li>The Name filter supports direct matches as well as regular expressions. Press Enter to add your regex to the list of search options</li>
+    <li>The Name filter dropdown has the names of all cards that match your other filters</li>
+    <li>You can also type a partial string (or regular expression) into the Name Search field</li>
     <li>You will only see cards that match ALL your filters</li>
     <li>Set, Card Type, and Symbol filters allow multiple selections, and show cards that match ANY of those selected</li>
     <li>Two Symbol filters allow you to combine AND / OR matching</li>
     <li>For example, to find cards with Earth AND either Life OR Order, select Earth with one, and Life + Order in the other</li>
+    <li>All cards should have names and resource symbols, but other properties (especially text) are more limited</li>
     <p>Deckbuilding</p>
     <li>On the search/home page, click on a card to add a copy to your deck; rightclick to remove</li>
     <li>You cannot add cards to more than 4 copies; if you remove the last copy, the card will leave your deck entirely</li>
@@ -21,5 +23,6 @@
         <li>Playtest solely includes cards released for Community Playtest on Tabletop Simulator. Updated 28 Nov 2020</li>
         <li>Universal theoretically starts with ShadoWar, but I didn't bother loading those sets. It only includes a few very old promos</li>
     </ul>
+    <p>Contact bro#8018 on Discord if you see a problem; have a feature request; or would like to help!</p>
   </div>
 </template>
